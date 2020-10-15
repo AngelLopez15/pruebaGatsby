7b39068f92433e6f69b7fd0db0f3c7d895a83ac7
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-10-15 13:41:30 -0500

    Agregado pluggind de sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,13 @@ import { Button } from 'react-bootstrap'
 import React from 'react'
 import Seo from '../components/Seo'
 import LayoutBasic from '../layouts/LayoutBasic'
-
+import './index.scss'
 
 export default function Index() {
   return (
     <LayoutBasic>
       <Seo title = "Hola Home" />
-      <h2>Este es el Home</h2>
+      <h2 className="title">Este es el Home</h2>
       <Button variant="primary" size="lg" block>
         Block level button
       </Button>
